5419279c39b3c3ba866069620c1e94a5c6f4f243
Musaddiq <contact@durranilab.com>
2021-02-08 14:58:30 +0530

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(13, 62);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(75, 50);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Text = "1";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(13, 13);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(478, 20);
+            this.Output.TabIndex = 1;
+            this.Output.Text = "0";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(94, 62);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 50);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(175, 62);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 50);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(301, 62);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(75, 50);
+            this.Plus.TabIndex = 4;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(382, 62);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(75, 50);
+            this.Minus.TabIndex = 5;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 268);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+
+        double PreviousValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            PreviousValue = double.Parse(Output.Text);
+            Output.Text = "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            PreviousValue = double.Parse(Output.Text);
+            Output.Text = "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            PreviousValue = double.Parse(Output.Text);
+            Output.Text = "3";
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            Calculate("PLUS");
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            Calculate("MINUS");
+        }
+
+        private void Calculate(string s)
+        {
+            if (s.Equals("PLUS"))
+            {
+                double total = PreviousValue + double.Parse(Output.Text);
+                Output.Text = total.ToString();
+            }
+            else if (s.Equals("MINUS"))
+            {
+                double total = PreviousValue - double.Parse(Output.Text);
+                Output.Text = total.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] parts = Output.Text.Split('+');
+            int total = 0;
+            foreach (string v in parts) {
+                total = total + Convert.ToInt32(v);
+            }
+            Output.Text = total.ToString();
+        }
+    }
+}
